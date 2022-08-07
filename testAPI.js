c283b18c391b7496d7ef2d3a784d60a961b38ef2
RaphaelPittet <pittetr.96@gmail.com>
2022-08-07 15:00:28 +0200

    express work - first API endpoint working

---
+++ testAPI.js
@@ -0,0 +1,15 @@
+const axios = require('axios').default;
+
+// Make a request
+axios.get('http://localhost:3000/')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
