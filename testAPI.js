c03a6ab125e383b2d6fa105974992bc21aa74311
RaphaelPittet <pittetr.96@gmail.com>
2022-08-07 21:40:20 +0200

    test protocole for Express API work, but can be completed

--- testAPI.js
+++ testAPI.js
@@ -1,15 +1,106 @@
 const axios = require('axios').default;
+const FormData = require('form-data');
+const fs = require('fs');
 
-// Make a request
-axios.get('http://localhost:3000/')
-  .then(function (response) {
-    // handle success
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+// init some test param
+let testParam1 = {
+  filamentType: "PLA",
+  printSettings: "10mm",
+  printerType: "ender3-V2",
+  fillDensity: ".40",
+  fileName: "Monkey_astronaut",
+  scalePercent: "150"
+}
+
+
+let testParamWithoutfileName = {
+  filamentType: "PLA",
+  printSettings: "10mm",
+  printerType: "ender3-V2",
+  fillDensity: ".40",
+  fileName: "",
+  scalePercent: "150"
+}
+
+let testParam2 = {
+  filamentType: "",
+  printSettings: "10mm",
+  printerType: "ender3-V2",
+  fillDensity: ".40",
+  fileName: "watch4-stand",
+  scalePercent: "150"
+}
+
+let testParam3 = {
+  filamentType: "PLA",
+  printSettings: "20mm",
+  printerType: "ender3-V2",
+  fillDensity: ".40",
+  fileName: "Monkey_astronaut",
+  scalePercent: "150"
+}
+
+let testParam4 = {
+  filamentType: "PLA",
+  printSettings: "20mm",
+  printerType: "ender3-V2",
+  fillDensity: ".10",
+  fileName: "entonoir",
+  scalePercent: "88"
+}
+
+function initFormData(slicingPram){
+  let tmpFormData = new FormData();
+  for (const property in slicingPram) {
+    tmpFormData.append(property, slicingPram[property]);
+  }
+  return tmpFormData
+}
+
+
+function generateRequestWithoutFile(slicingParam){
+
+  // fill formData variable with slicing param and provided file
+  let formData = initFormData(slicingParam);
+  // launch post request
+  axios.post('http://localhost:3000/upload', formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      maxBodyLength: Infinity
+    })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+}
+
+
+
+function generateRequest(slicingParam){
+
+  // fill formData variable with slicing param and provided file
+  let formData = initFormData(slicingParam);
+  formData.append('file', fs.createReadStream(`./test-files/${slicingParam.fileName}.stl`));
+
+  // launch post request
+  axios.post('http://localhost:3000/upload', formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      maxBodyLength: Infinity
+    })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+}
+
+generateRequest(testParam1);
+generateRequestWithoutFile(testParam2);
+//generateRequest(testParam3);
+//generateRequest(testParamWithoutfileName);
